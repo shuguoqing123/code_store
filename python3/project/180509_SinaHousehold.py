e21b92d45d228819cf230eeef077cd41c8db61b8
shuguoqing123 <1053869565@qq.com>
2018-05-09 23:26:07 +0800

    Update 180509_SinaHousehold.py

--- python3/project/180509_SinaHousehold.py
+++ python3/project/180509_SinaHousehold.py
@@ -1,1 +1,9 @@
+from urllib import request
+from urllib import parse
+import urllib
+
+
+req = request.Request('http://bbs.jiaju.com')
+rep = request.urlopen(req)
+open('debug.txt','w',encoding='utf-8').write(rep.read().decode('gbk','ignore').encode('utf-8').decode('utf-8'))
 
